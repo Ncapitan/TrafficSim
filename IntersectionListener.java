b301adb427bb3d08ae948584197fd3db65483700
PickardNicko <npickard@student.neumont.edu>
2015-01-29 16:52:17 -0700

    Create IntersectionListener.java

---
+++ IntersectionListener.java
@@ -0,0 +1,17 @@
+  package edu.csc150;
+
+public interface IntersectionListener{
+
+	interface intersectionListener{
+		
+		public void approachingIntersection();
+		
+		public void enteringIntersection();
+		
+		public void LeavingIntersection();
+		
+		public void gone();
+		
+		}
+
+}
